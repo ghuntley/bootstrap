f48dd44279cfb9e5cb7445ff9ad8aa85339b792d
Geoffrey Huntley <ghuntley@ghuntley.com>
2016-05-03 09:10:46 +1000

    wip

--- src/Services.Connected/NetworkConnectivity/NetworkConnectivityService.cs
+++ src/Services.Connected/NetworkConnectivity/NetworkConnectivityService.cs
@@ -1,13 +1,51 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reactive.Linq;
+using System.Reactive.Threading.Tasks;
 using System.Text;
 using System.Threading.Tasks;
+using Plugin.Connectivity;
+using Plugin.Connectivity.Abstractions;
 using ReactiveSearch.Services.NetworkConnectivity;
+using ReactiveSearch.Utility;
 
 namespace ReactiveSearch.Services.Connected.NetworkConnectivity
 {
-    public class NetworkConnectivityService : INetworkConnectivityService
+    public class NetworkConnectivityService : DisposableBase, INetworkConnectivityService
     {
+        public NetworkConnectivityService()
+        {
+            CrossConnectivity.Current.ConnectivityChanged += OnConnectivityChanged;
+        }
+
+        public IObservable<IEnumerable<ConnectionType>> Connectivity()
+        {
+            return Observable.Return(CrossConnectivity.Current.ConnectionTypes);
+        }
+
+        public IObservable<bool> IsInternetConnectivityAvailable()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IObservable<bool> IsReachable(string host, int port = 80, int msTimeout = 5000)
+        {
+            return CrossConnectivity.Current.IsRemoteReachable(host, port, msTimeout).ToObservable();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+            if (disposing)
+            {
+                CrossConnectivity.Current.ConnectivityChanged -= OnConnectivityChanged;
+            }
+        }
+
+        void OnConnectivityChanged(object sender, ConnectivityChangedEventArgs e)
+        {
+
+        }
     }
 }
--- src/Services/NetworkConnectivity/INetworkConnectivityService.cs
+++ src/Services/NetworkConnectivity/INetworkConnectivityService.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Plugin.Connectivity.Abstractions;
 
 namespace ReactiveSearch.Services.NetworkConnectivity
 {
     public interface INetworkConnectivityService
     {
+        IObservable<bool> IsInternetConnectivityAvailable();
+        IObservable<bool> IsReachable(string host, int port = 80, int msTimeout = 5000);
+        IObservable<IEnumerable<ConnectionType>> Connectivity();
     }
 }
